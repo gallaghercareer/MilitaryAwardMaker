95059694c399cc52babbf92bd720f0b1c54e4f04
John Gallagher <gallaghercareer@gmail.com>
2023-12-29 09:23:19 -0500

    update

--- militaryawardwriter/src/Components/CallLambda.js
+++ militaryawardwriter/src/Components/CallLambda.js
@@ -4,26 +4,39 @@ import Cookies from 'js-cookie';
 const CallLambda = () => {
     const [user_message, setUserMessage] = useState('');
 
-    const callOpenAI = async (userMessage) => {
-        const url = 'https://csitg9jz5c.execute-api.us-east-2.amazonaws.com/default/sendMessage_AAM';   
-        const headers = new Headers();
+    const callOpenAI = async (event) => {
+        event.preventDefault();
+        const url = 'https://csitg9jz5c.execute-api.us-east-2.amazonaws.com/beta/sendMessage_AAM';   
         const authorizationToken = Cookies.get('jwt');
         const thread_Id = Cookies.get("thread_Id") || '';
-        headers.append('Authorization', authorizationToken);
-        headers.append('Thread_id', thread_Id);
-        headers.append('User_message', userMessage);
-    
+        
+        const headers = {
+            'Authorization': `Bearer ${authorizationToken}`,
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest'
+        };
+     
+
         try {
+            const requestBody = {
+                body: JSON.stringify({
+                    thread_id: thread_Id,
+                    user_message: user_message
+                })
+            };
             const response = await fetch(url, {
                 method: 'POST', // or 'POST', 'PUT', 'DELETE', etc., depending on your needs
-                headers: headers
+                headers: headers,
+                body:  requestBody
+              
             });
     
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
     
-            const data = await response.json(); // or response.text() if the response is not in JSON format
+            let data = await response.json(); // or response.text() if the response is not in JSON format
+            data = JSON.stringify(data);
             console.log("lambda response:" + data)
             //set thread_Id in cookies here
             return data;
