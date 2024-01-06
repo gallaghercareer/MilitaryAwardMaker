317bc2085e9aca12d1a59986ae42c735660c97ba
John Gallagher <gallaghercareer@gmail.com>
2024-01-05 20:26:40 -0500

    updated temp landing page

--- militaryawardwriter/src/Pages/Landing.js
+++ militaryawardwriter/src/Pages/Landing.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Correct import for useNavigate
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { CircularProgress, AppBar, Link, InputAdornment, TextField, IconButton, Toolbar, Button, Stack, Box, ButtonGroup,  MenuItem, Menu, Icon } from '@mui/material';
 
 function Landing() {
+   
+    const navigate = useNavigate();
 
- 
+    const navigateToCoastGuardHomepage = () =>{
+        navigate('/coastguard');
+    };
+    
     return (
-
-        <div>
-        <h1>Landing Page</h1>
-        </div>        );
+        <>
+        <Grid container sx={{paddingTop:'5rem'}}>
+            
+            <Grid item xs={12} sm={12} md={3} lg={3}> 
+            <Typography onClick={navigateToCoastGuardHomepage}><Button>Coastguard</Button></Typography>
+            </Grid>
+           {/*
+            <Grid item xs={12} sm={12} md={3} lg={3}> 
+            <Typography onClick={navigateToCoastGuardHomepage}>Army</Typography>
+            </Grid>
+            
+            <Grid item xs={12} sm={12} md={3} lg={3}> 
+            <Typography onClick={navigateToCoastGuardHomepage}>Navy</Typography>
+            </Grid>
+            
+            <Grid item xs={12} sm={12} md={3} lg={3}> 
+            <Typography onClick={navigateToCoastGuardHomepage}>Airforce</Typography>
+            </Grid>
+          
+    */}
+        </Grid>
+        </>
+    );
 }
 
 export default Landing;
